87a4a31188768dd2415e7929422fd8b0f205e8e8
Arindam Biswas <arindam.biswas@gmail.com>
2019-09-27 14:39:00 +0530

    Finished Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/arindam_profile_pic.png'),
+            ),
+            Text(
+              'Arindam Biswas',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  color: Colors.teal.shade100, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade900,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade600,
+                  ),
+                  title: Text(
+                    '+91 7875757782',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0),
+                  )),
+            ),
+            Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'arindam.biswas@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ))
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
